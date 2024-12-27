5adc522b1945eb8c1132ff177d848b6c9e44e7d8
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-27 14:27:03 +0530

    Add name, role, and image

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+st.title("My company website")
+content1 = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+"""
+st.write(content1)
+
+df = pd.read_csv("data.csv", sep=",")
+
+col1, col2, col3, col4, col5 = st.columns([1.5, 0.5, 1.5, 0.5, 1.5])
+
+with col1:
+    for index, row in df[:4].iterrows():
+        st.header(f"{row['first name']} {row['last name']}")
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+
+with col3:
+    for index, row in df[4:8].iterrows():
+        st.header(f"{row['first name']} {row['last name']}")
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+
+with col5:
+    for index, row in df[8:].iterrows():
+        st.header(f"{row['first name']} {row['last name']}")
+        st.write(row["role"])
+        st.image("images/" + row["image"])
