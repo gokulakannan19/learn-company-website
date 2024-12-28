b53423068dcfa03797e49230f5da8f1ff4364ccc
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-28 13:02:50 +0530

    send email function added and integrated with forms

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from send_email import  send_email
 
 st.title("Contact Us")
 topics_list = ["Job Proposals", "Enquiry", "Others"]
@@ -7,6 +8,16 @@ with st.form(key="email_form"):
     user_email = st.text_input("Your Email address")
     topic = st.selectbox("Select the topic you want to discuss", topics_list)
     raw_message = st.text_area("Message")
+    message = f"""\
+Subject: Company Website
+
+From: {user_email}
+
+Topic: {topic}
+
+{raw_message}
+"""
     button = st.form_submit_button("Submit")
     if button:
-        print("Pressed")
+        send_email(message)
+        st.info("Email send successfully")
--- send_email.py
+++ send_email.py
@@ -1,8 +1,20 @@
-# import smtplib
-# import ssl
-#
-#
-# def send_email(message):
-#     host = "smtp.gmail.com"
-#     port =
-#     username = "goks"
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "####"
+    password = "####"
+
+    context = ssl.create_default_context()
+    receiver = "####"
+
+    try:
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+
+    except Exception as e:
+        print(f"Error sending email: {e}")
