964969dfe7e56b886a2bb353adea6c95fb48a3e3
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-28 13:06:58 +0530

    topic fetched from csv

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,12 +1,13 @@
+import pandas as pd
 import streamlit as st
 from send_email import  send_email
 
 st.title("Contact Us")
-topics_list = ["Job Proposals", "Enquiry", "Others"]
+topics = pd.read_csv("topics.csv")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your Email address")
-    topic = st.selectbox("Select the topic you want to discuss", topics_list)
+    topic = st.selectbox("Select the topic you want to discuss", topics["topic"])
     raw_message = st.text_area("Message")
     message = f"""\
 Subject: Company Website
